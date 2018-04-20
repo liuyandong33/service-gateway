eff6467a9af89bc46510594e9d938ff4b926353f
liuyandong <554030404@qq.com>
2018-04-20 23:04:46 +0800

    初始化网关项目！！

--- src/main/java/build/dream/zuul/Application.java
+++ src/main/java/build/dream/zuul/Application.java
@@ -2,14 +2,14 @@ package build.dream.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * Created by liuyandong on 2018/4/15.
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableZuulProxy
 public class Application {
     public static void main(String[] args) {
