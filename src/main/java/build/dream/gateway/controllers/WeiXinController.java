b21cae049e16ecd6db08a6a5dd172f08361d7b81
liuyandong <554030404@qq.com>
2018-05-29 22:07:43 +0800

    修改包名！！

---
+++ src/main/java/build/dream/gateway/controllers/WeiXinController.java
@@ -0,0 +1,71 @@
+package build.dream.gateway.controllers;
+
+import build.dream.common.beans.WeiXinOAuthAccessToken;
+import build.dream.common.beans.WeiXinUserInfo;
+import build.dream.common.constants.Constants;
+import build.dream.common.utils.*;
+import build.dream.gateway.models.weixin.ObtainUserInfoModel;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping(value = "/weiXin")
+public class WeiXinController {
+    @RequestMapping(value = "/obtainUserInfo", method = RequestMethod.GET)
+    public String obtainUserInfo() throws InstantiationException, IllegalAccessException, ParseException, NoSuchFieldException, IOException {
+        Map<String, String> requestParameters = ApplicationHandler.getRequestParameters();
+        ObtainUserInfoModel obtainUserInfoModel = ApplicationHandler.instantiateObject(ObtainUserInfoModel.class, requestParameters);
+        obtainUserInfoModel.validateAndThrow();
+
+        String appId = obtainUserInfoModel.getAppId();
+        String scope = obtainUserInfoModel.getScope();
+        String redirectUri = obtainUserInfoModel.getRedirectUri();
+        String state = obtainUserInfoModel.getState();
+
+        Map<String, String> parameters = new HashMap<String, String>();
+        parameters.put("redirectUri", redirectUri);
+        parameters.put("appId", appId);
+
+        String outsideUrl = SystemPartitionUtils.getOutsideUrl(Constants.SERVICE_NAME_GATEWAY, "weiXin", "obtainUserInfo");
+        String authorizeUrl = WeiXinUtils.generateAuthorizeUrl(appId, scope, outsideUrl + "?redirectUri=" + WebUtils.buildQueryString(parameters), state);
+        return "redirect:" + authorizeUrl;
+    }
+
+    @RequestMapping(value = "/obtainUserInfo", method = RequestMethod.GET)
+    public String oauthCallback() throws IOException {
+        Map<String, String> requestParameters = ApplicationHandler.getRequestParameters();
+        String code = requestParameters.get("code");
+        String redirectUri = requestParameters.get("redirectUri");
+        String appId = requestParameters.get("appId");
+        String secret = "";
+
+        WeiXinOAuthAccessToken weiXinOAuthAccessToken = WeiXinUtils.obtainOAuthAccessToken(appId, secret, code);
+
+        Map<String, String> parameters = new HashMap<String, String>();
+
+        String openId = weiXinOAuthAccessToken.getOpenId();
+        parameters.put("openId", openId);
+
+        String scope = weiXinOAuthAccessToken.getScope();
+        if (Constants.SNSAPI_BASE.equals(scope)) {
+
+        } else if (Constants.SNSAPI_USERINFO.equals(scope)) {
+            WeiXinUserInfo weiXinUserInfo = WeiXinUtils.obtainUserInfo(weiXinOAuthAccessToken.getAccessToken(), openId, null);
+            parameters.put("userInfo", GsonUtils.toJson(weiXinUserInfo));
+        }
+
+        StringBuilder url = new StringBuilder(redirectUri);
+        if (redirectUri.indexOf("?") >= 0) {
+            url.append("&");
+        } else {
+            url.append("?");
+        }
+        return "redirect:" + url.append(WebUtils.buildQueryString(parameters)).toString();
+    }
+}
