a0b7105df7b500cb8e5c3b10e390d65df8d3077e
liuyandong <554030404@qq.com>
2018-08-02 14:28:19 +0800

    增加异步通知统一处理接口！！

--- src/main/java/build/dream/gateway/constants/Constants.java
+++ src/main/java/build/dream/gateway/constants/Constants.java
@@ -6,4 +6,8 @@ public class Constants extends build.dream.common.constants.Constants {
     public static final String WEI_XIN_PAY_CALLBACK_FAILURE_RETURN_VALUE = "<xml><return_code><![CDATA[FAILURE]]></return_code></xml>";
     public static final String ELEME_ORDER_CALLBACK_SUCCESS_RETURN_VALUE = "{\"message\":\"ok\"}";
     public static final String ELEME_MESSAGE_TOPIC = "eleme.message.topic";
+
+    public static final String MEI_TUAN_MESSAGE_TOPIC = "mei.tuan.message.topic";
+
+    public static final String MEI_TUAN_CALLBACK_SUCCESS_RETURN_VALUE = "{\"data\": \"OK\"}";
 }
