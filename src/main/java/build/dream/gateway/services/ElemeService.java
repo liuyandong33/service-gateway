44ac0500d2d2f529fe6074b6f393c44feaa152d2
liuyandong <554030404@qq.com>
2018-12-21 21:02:09 +0800

    修改包名！！

--- src/main/java/build/dream/gateway/services/ElemeService.java
+++ src/main/java/build/dream/gateway/services/ElemeService.java
@@ -4,7 +4,6 @@ import build.dream.common.saas.domains.Tenant;
 import build.dream.common.utils.*;
 import build.dream.gateway.constants.Constants;
 import build.dream.gateway.mappers.TenantMapper;
-import net.sf.json.JSONObject;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang.Validate;
@@ -25,26 +24,26 @@ import java.util.concurrent.TimeUnit;
 public class ElemeService {
     @Transactional(readOnly = true)
     public String handleCallback(String callbackRequestBody) throws ExecutionException, InterruptedException {
-        JSONObject callbackRequestBodyJsonObject = JSONObject.fromObject(callbackRequestBody);
-        Validate.isTrue(ElemeUtils.checkSignature(callbackRequestBodyJsonObject, ConfigurationUtils.getConfiguration(Constants.ELEME_APP_SECRET)), "签名校验未通过！");
+        Map<String, Object> callbackRequestBodyMap = JacksonUtils.readValueAsMap(callbackRequestBody, String.class, Object.class);
+        Validate.isTrue(ElemeUtils.checkSignature(callbackRequestBodyMap, ConfigurationUtils.getConfiguration(Constants.ELEME_APP_SECRET)), "签名校验未通过！");
 
-        String uuid = DigestUtils.md5Hex(callbackRequestBodyJsonObject.getString("message"));
+        String uuid = DigestUtils.md5Hex(MapUtils.getString(callbackRequestBodyMap, "message"));
         String key = "eleme_callback_sign_" + uuid;
         boolean setnxSuccessful = CacheUtils.setnx(key, key);
         String handleResult = null;
         if (setnxSuccessful) {
-            handleResult = handleCallback(key, uuid, callbackRequestBodyJsonObject);
+            handleResult = handleCallback(key, uuid, callbackRequestBodyMap);
         } else {
             handleResult = Constants.ELEME_ORDER_CALLBACK_SUCCESS_RETURN_VALUE;
         }
         return handleResult;
     }
 
-    private String handleCallback(String key, String uuid, JSONObject callbackRequestBodyJsonObject) throws ExecutionException, InterruptedException {
+    private String handleCallback(String key, String uuid, Map<String, Object> callbackRequestBodyMap) throws ExecutionException, InterruptedException {
         String handleResult = null;
         try {
             CacheUtils.expire(key, 1800, TimeUnit.SECONDS);
-            BigInteger shopId = BigInteger.valueOf(callbackRequestBodyJsonObject.getLong("shopId"));
+            BigInteger shopId = BigInteger.valueOf(MapUtils.getLongValue(callbackRequestBodyMap, "shopId"));
 
             Map<String, Object> tenantInfo = DatabaseHelper.callMapperMethod(TenantMapper.class, "obtainTenantInfo", TupleUtils.buildTuple2(BigInteger.class, shopId));
             if (MapUtils.isEmpty(tenantInfo)) {
@@ -53,7 +52,7 @@ public class ElemeService {
                 String partitionCode = MapUtils.getString(tenantInfo, Tenant.FieldName.PARTITION_CODE);
                 Map<String, Object> elemeMessage = new HashMap<String, Object>();
                 elemeMessage.put("uuid", uuid);
-                elemeMessage.put("callbackRequestBody", callbackRequestBodyJsonObject);
+                elemeMessage.put("callbackRequestBody", callbackRequestBodyMap);
                 elemeMessage.put("count", 10);
 
                 String topic = partitionCode + "_" + ConfigurationUtils.getConfiguration(Constants.ELEME_MESSAGE_TOPIC);
