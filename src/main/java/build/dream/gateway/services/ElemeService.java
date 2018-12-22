95399c87dc6f20ff6e015e0087a188beba730e42
liuyandong <554030404@qq.com>
2018-12-22 21:38:36 +0800

    修改包名！！

--- src/main/java/build/dream/gateway/services/ElemeService.java
+++ src/main/java/build/dream/gateway/services/ElemeService.java
@@ -48,11 +48,13 @@ public class ElemeService {
             if (MapUtils.isEmpty(tenantInfo)) {
                 handleResult = Constants.ELEME_ORDER_CALLBACK_SUCCESS_RETURN_VALUE;
             } else {
+                BigInteger tenantId = BigInteger.valueOf(MapUtils.getLongValue(tenantInfo, Tenant.FieldName.ID));
                 String partitionCode = MapUtils.getString(tenantInfo, Tenant.FieldName.PARTITION_CODE);
                 Map<String, Object> elemeMessage = new HashMap<String, Object>();
                 elemeMessage.put("uuid", uuid);
                 elemeMessage.put("callbackRequestBody", callbackRequestBodyMap);
                 elemeMessage.put("count", 10);
+                elemeMessage.put("tenantId", tenantId);
 
                 String topic = partitionCode + "_" + ConfigurationUtils.getConfiguration(Constants.ELEME_MESSAGE_TOPIC);
                 ListenableFuture<SendResult<String, String>> listenableFuture = KafkaUtils.send(topic, uuid, GsonUtils.toJson(elemeMessage));
