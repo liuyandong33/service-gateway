8ebd98a5ba07365812c40e75ce2b4d10b1703495
liuyandong <554030404@qq.com>
2019-07-01 15:19:02 +0800

    build 脚本修改！！

---
+++ src/main/java/build/dream/gateway/controllers/JDDJController.java
@@ -0,0 +1,36 @@
+package build.dream.gateway.controllers;
+
+import build.dream.common.utils.ApplicationHandler;
+import build.dream.common.utils.CommonRedisUtils;
+import build.dream.common.utils.JacksonUtils;
+import build.dream.gateway.constants.Constants;
+import build.dream.gateway.services.DJSWService;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping(value = "/jddj")
+public class JDDJController {
+    @Autowired
+    private DJSWService djswService;
+
+    @RequestMapping(value = "/callback")
+    @ResponseBody
+    public String callback() {
+        Map<String, String> requestParameters = ApplicationHandler.getRequestParameters();
+        String token = requestParameters.get("token");
+        if (StringUtils.isNotBlank(token)) {
+            Map<String, Object> tokenMap = JacksonUtils.readValueAsMap(token, String.class, Object.class);
+            String venderId = MapUtils.getString(tokenMap, "venderId");
+            CommonRedisUtils.hset(Constants.KEY_JDDJ_TOKENS, venderId, JacksonUtils.writeValueAsString(tokenMap));
+        }
+
+        return djswService.buildSuccessResult();
+    }
+}
