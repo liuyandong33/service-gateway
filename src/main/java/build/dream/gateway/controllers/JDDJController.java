ed8a34cd80963b0fa791f135901d19c3c7d10e0e
liuyandong <554030404@qq.com>
2019-07-02 21:42:44 +0800

    修改包名！！

--- src/main/java/build/dream/gateway/controllers/JDDJController.java
+++ src/main/java/build/dream/gateway/controllers/JDDJController.java
@@ -2,12 +2,11 @@ package build.dream.gateway.controllers;
 
 import build.dream.common.utils.ApplicationHandler;
 import build.dream.common.utils.CommonRedisUtils;
+import build.dream.common.utils.JDDJUtils;
 import build.dream.common.utils.JacksonUtils;
 import build.dream.gateway.constants.Constants;
-import build.dream.gateway.services.DJSWService;
 import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,9 +16,6 @@ import java.util.Map;
 @Controller
 @RequestMapping(value = "/jddj")
 public class JDDJController {
-    @Autowired
-    private DJSWService djswService;
-
     @RequestMapping(value = "/callback")
     @ResponseBody
     public String callback() {
@@ -31,6 +27,6 @@ public class JDDJController {
             CommonRedisUtils.hset(Constants.KEY_JDDJ_TOKENS, venderId, JacksonUtils.writeValueAsString(tokenMap));
         }
 
-        return djswService.buildSuccessResult();
+        return JDDJUtils.buildSuccessResult();
     }
 }
--- src/main/java/build/dream/gateway/services/DJSWService.java
+++ src/main/java/build/dream/gateway/services/DJSWService.java
@@ -1,9 +1,6 @@
 package build.dream.gateway.services;
 
-import build.dream.common.utils.CommonRedisUtils;
-import build.dream.common.utils.ConfigurationUtils;
-import build.dream.common.utils.JacksonUtils;
-import build.dream.common.utils.KafkaUtils;
+import build.dream.common.utils.*;
 import build.dream.gateway.constants.Constants;
 import org.apache.commons.collections.MapUtils;
 import org.springframework.stereotype.Service;
@@ -29,41 +26,9 @@ public class DJSWService {
             String partitionCode = MapUtils.getString(infoMap, "partitionCode");
             String topic = partitionCode + "_" + ConfigurationUtils.getConfiguration(Constants.JDDJ_MESSAGE_TOPIC);
             KafkaUtils.send(topic, JacksonUtils.writeValueAsString(message));
-            return buildSuccessResult();
+            return JDDJUtils.buildSuccessResult();
         } catch (Exception e) {
-            return buildFailureResult(e.getMessage());
+            return JDDJUtils.buildFailureResult(e.getMessage());
         }
-    }
-
-    public String buildResult(String code, String msg, String data) {
-        Map<String, Object> result = new HashMap<String, Object>();
-        result.put("code", code);
-        result.put("msg", msg);
-        result.put("data", data);
-        return JacksonUtils.writeValueAsString(result);
-    }
-
-    public String buildSuccessResult() {
-        return buildResult("0", "success", "");
-    }
-
-    public String buildSuccessResult(String msg) {
-        return buildResult("0", msg, "");
-    }
-
-    public String buildSuccessResult(String msg, String data) {
-        return buildResult("0", msg, data);
-    }
-
-    public String buildFailureResult() {
-        return buildResult("-1", "failure", "");
-    }
-
-    public String buildFailureResult(String msg) {
-        return buildResult("-1", msg, "");
-    }
-
-    public String buildFailureResult(String msg, String data) {
-        return buildResult("-1", msg, data);
     }
 }
