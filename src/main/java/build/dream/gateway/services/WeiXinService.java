669b56e1704fe5cfcd50f18c58943527d85fc4dc
liuyandong <554030404@qq.com>
2019-08-23 09:58:29 +0800

    错误修改！！

--- src/main/java/build/dream/gateway/services/WeiXinService.java
+++ src/main/java/build/dream/gateway/services/WeiXinService.java
@@ -92,7 +92,7 @@ public class WeiXinService {
     }
 
     private byte[] decode(byte[] decrypted) {
-        int pad = (int) decrypted[decrypted.length - 1];
+        int pad = decrypted[decrypted.length - 1];
         if (pad < 1 || pad > 32) {
             pad = 0;
         }
