14ef8ed868fb56c2aa90bd731190a9fca00bd605
liuyandong <554030404@qq.com>
2020-03-04 02:48:59 +0800

    修改包名！！

--- src/main/java/build/dream/gateway/Application.java
+++ src/main/java/build/dream/gateway/Application.java
@@ -3,6 +3,7 @@ package build.dream.gateway;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -12,7 +13,7 @@ import org.springframework.kafka.annotation.EnableKafka;
  * Created by liuyandong on 2018/4/15.
  */
 @ServletComponentScan
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
 @EnableZuulProxy
 @EnableKafka
